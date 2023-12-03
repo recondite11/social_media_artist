e8e6404d80c76506049395e377d2dd44fa22fa30
recondite11 <kartikpal1402@gmail.com>
2023-12-03 23:41:28 +0530

    Project ready but not actually ready.

--- script.js
+++ script.js
@@ -1,15 +1,101 @@
 // NAVIGATION BAR TOGGLER LOGIC
-let mobilemenu= document.getElementById('mobile-menu')
-mobilemenu.addEventListener('click', function () {
+function navBarToggler(){
+let mobileMenu= document.getElementById('mobile-menu')
+mobileMenu.addEventListener('click', function () {
     let nav= document.getElementById('nav');
     let menu= document.getElementById("menu");
-    let rightnav= document.getElementById('right')
+    let rightNav= document.getElementById('right')
     nav.classList.toggle('resp-nav')
     menu.classList.toggle('resp-menu')
-    rightnav.classList.toggle('resp-right-nav')
+    rightNav.classList.toggle('resp-right-nav')
+});
+}
+navBarToggler()
+
+// LOGIN FORM
+function login() {
+    var loginUserID = document.getElementById('loginUserID').value;
+    var loginPassword = document.getElementById('loginPassword').value;
+    if (loginUserID == 'user123' && loginPassword == 'pass123') {
+        // Set a flag in localStorage to indicate successful login
+        localStorage.setItem('isLoggedIn', 'true');
+        // Redirect to the main page
+        window.location.href = 'index.html';
+    } else {
+        // Handle unsuccessful login
+        alert('Login failed. Please check your credentials.');
+    }
+}
+
+function logout() {
+    // Clear the login status from localStorage
+    localStorage.removeItem('isLoggedIn');
+    // Redirect to the main page
+    window.location.href = 'index.html';
+}
+
+// Check login status on page load
+document.addEventListener('DOMContentLoaded', function () {
+    var isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (isLoggedIn === 'true') {
+        // Hide the login and signup buttons
+        var loginButton = document.getElementById('login');
+        var signupButton = document.getElementById('signup');
+        document.getElementById('post').style.display = 'flex';
+        document.getElementById('logout').style.display = 'flex';
+        if (loginButton) {
+            loginButton.style.display = 'none';
+        }
+        if (signupButton) {
+            signupButton.style.display = 'none';
+        }
+    }
 });
 
+
+
+// SIGN UP FORM 
+function signup() {
+    var successfulSignup = true;
+    if (successfulSignup) {
+        // Set a flag in localStorage to indicate successful signup
+        localStorage.setItem('isLoggedIn', 'true');
+        // Redirect to the main page
+        window.location.href = 'index.html';
+    } else {
+        // Handle unsuccessful signup
+        alert('Signup failed. Please try again.');
+    }
+}
+
+function logout() {
+    // Clear the login and signup status from localStorage
+    localStorage.removeItem('isLoggedIn');
+    // Redirect to the main page
+    window.location.href = 'index.html';
+}
+
+// Check login and signup status on page load
+document.addEventListener('DOMContentLoaded', function () {
+    var isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (isLoggedIn === 'true') {
+        // Hide the login and signup buttons
+        var loginButton = document.getElementById('login');
+        var signupButton = document.getElementById('signup');
+        document.getElementById('post').style.display = 'flex';
+        document.getElementById('logout').style.display = 'flex';
+        if (loginButton) {
+            loginButton.style.display = 'none';
+        }
+        if (signupButton) {
+            signupButton.style.display = 'none';
+        }
+    }
+});
+
+
 // NAVIGATION BAR TAB SWITCH LOGIC 
+function navTabSwitch(){
 let currentLocation= window.location.href;
 let tabs= document.querySelectorAll('#menu a');
 for(i=0; i<tabs.length; i++){
@@ -19,85 +105,121 @@ for(i=0; i<tabs.length; i++){
         tab.classList.add('active')
     }
 }
-console.log(currentLocation)
-// HOVER EFFECT ON POSTS 
-let postimgs= document.getElementsByClassName('post-img');
-let posthovers= document.getElementsByClassName('post-hover');
-for(i=0; i<postimgs.length; i++){
-    let postimg= postimgs[i];
-    let posthover= posthovers[i];
-    postimg.addEventListener('mouseover', function(){
-        posthover.style.visibility= 'visible'
+}
+navTabSwitch()
+
+// HOVER EFFECT ON POSTS
+function postHover(){
+let postImgs= document.getElementsByClassName('post-img');
+let postHovers= document.getElementsByClassName('post-hover');
+for(i=0; i<postImgs.length; i++){
+    let postImg= postImgs[i];
+    let postHover= postHovers[i];
+    postImg.addEventListener('mouseover', function(){
+        postHover.style.visibility= 'visible'
     })
-    postimg.addEventListener('mouseout', function(){
-        posthover.style.visibility= 'hidden'
+    postImg.addEventListener('mouseout', function(){
+        postHover.style.visibility= 'hidden'
     })
 }
+}
+postHover()
+
 // LIKE BUTTON
-let likebtns= document.getElementsByClassName('like-btn');
-let likecounts= document.getElementsByClassName('like-count');
-for(i=0; i<likebtns.length; i++){
-    let likebtn= likebtns[i];
-    let likecount= likecounts[i];
+function likeButton(){
+let likeBtns= document.getElementsByClassName('like-btn');
+let likeCounts= document.getElementsByClassName('like-count');
+for(i=0; i<likeBtns.length; i++){
+    let likeBtn= likeBtns[i];
+    let likeCount= likeCounts[i];
     let like= false
-    likebtn.addEventListener('click', function(){
-        likebtn.classList.toggle('like-toggle')
-        likecount.classList.toggle('like-toggle')
-        let num= parseInt(likecount.innerHTML)
+    likeBtn.addEventListener('click', function(){
+        likeBtn.classList.toggle('like-toggle')
+        likeCount.classList.toggle('like-toggle')
+        let num= parseInt(likeCount.innerHTML)
         if(!like){
-            likecount.innerHTML= num + 1;
+            likeCount.innerHTML= num + 1;
             like= true
         }
         else{
-            likecount.innerHTML= num - 1;
+            likeCount.innerHTML= num - 1;
             like= false
         }
     })
 }
+}
+likeButton()
 
 // HOVER ON PROFILE NAME
-let profilecards= document.getElementsByClassName('profile-overview');
-let profilecontent= document.getElementsByClassName('profile-content');
-for(i=0; i<profilecards.length; i++){
-    let profilename= profilecontent[i].querySelector('a');
-    let profilecard= profilecards[i]
-    profilename.addEventListener('mouseover', function(){
-        profilecard.classList.add('profile-toggle')
+function profileHover(){
+let profileCards= document.getElementsByClassName('profile-overview');
+let profileContent= document.getElementsByClassName('profile-content');
+for(i=0; i<profileCards.length; i++){
+    let profileName= profileContent[i].querySelector('a');
+    let profileCard= profileCards[i]
+    profileName.addEventListener('mouseover', function(){
+        profileCard.classList.add('profile-toggle')
     })
-    profilename.addEventListener('mouseout', function(){
-        profilecard.classList.remove('profile-toggle')
+    profileName.addEventListener('mouseout', function(){
+        profileCard.classList.remove('profile-toggle')
     })
 }
+}
+profileHover()
 
-// LOGIN FORM 
-function login() {
-    var loginUserID = document.getElementById('loginUserID').value;
-    var loginPassword = document.getElementById('loginPassword').value;
-    var login = document.getElementById('login')
-    console.log(login)
-    var signup= document.getElementById('signup')
-    var post= document.getElementById('post')
-    // Check if loginUserID and loginPassword match a predefined value (for simplicity)
-    if (loginUserID === 'user123' && loginPassword === 'pass123') {
-        alert('Login successful!');
-        // console.log(login)
-        window.location.href = 'index.html';
-    } else {
-        alert('Login failed. Please check your credentials.');
+// CATEGORY CHANGE ON EXPLORE
+function categoryChange(){
+let categoryTabs = document.getElementsByClassName('category-tab');
+let categoryCards= document.getElementsByClassName('category-card');
+for (let i = 0; i < categoryTabs.length; i++) {
+    let categoryTab = categoryTabs[i];
+    let categoryCard = categoryCards[i];
+    categoryTab.addEventListener('click', function () {
+        for (let j = 0; j < categoryTabs.length; j++) {
+            categoryTabs[j].classList.remove('active-tab');
+            categoryCards[j].classList.remove('active-card');
+        }
+        categoryTab.classList.add('active-tab');
+        categoryCard.classList.add('active-card');
+    });
+}
+}
+categoryChange()
+
+// PROFILE CHANGE ON HIRE 
+function profileChange(){
+    let profileTogglers= document.getElementsByClassName('category-switch');
+    let profiles= document.getElementsByClassName('hire-profiles');
+    for (let i = 0; i < profileTogglers.length; i++) {
+        let profileToggler = profileTogglers[i];
+        let profile = profiles[i];
+        profileToggler.addEventListener('click', function () {
+            for (let j = 0; j < profileTogglers.length; j++) {
+                profileTogglers[j].classList.remove('active-btn');
+                profiles[j].classList.remove('active-profile');
+            }
+            profileToggler.classList.add('active-btn');
+            profile.classList.add('active-profile');
+        });
     }
-}
+    }
+    profileChange()
 
-// SIGN UP FORM
-function signup() {
-    var signupFirstName = document.getElementById('signupFirstName').value;
-    var signupLastName = document.getElementById('signupLastName').value;
-    var signupDOB = document.getElementById('signupDOB').value;
-    var signupUserID = document.getElementById('signupUserID').value;
-    var signupPassword = document.getElementById('signupPassword').value;
-
-    // Perform signup logic (for simplicity, just an alert in this example)
-    alert('User signed up!\n\n' +
-          'Name: ' + signupFirstName + ' ' + signupLastName + '\n' +
-          'Date of Birth: ' + signupDOB + '\n' +
-          'User ID: ' + signupUserID);
-}
+// JOB PROFILE CHANGE ON HIRE 
+function jobProfileChange(){
+    let jobProfileTogglers= document.querySelectorAll('.job-tab');
+    let jobProfiles= document.getElementsByClassName('job-cards');
+    for (let i = 0; i < jobProfileTogglers.length; i++) {
+        let jobProfileToggler = jobProfileTogglers[i];
+        let jobProfile = jobProfiles[i];
+        jobProfileToggler.addEventListener('click', function () {
+            for (let j = 0; j < jobProfileTogglers.length; j++) {
+                jobProfileTogglers[j].classList.remove('active-job-tab');
+                jobProfiles[j].classList.remove('active-job');
+            }
+            jobProfileToggler.classList.add('active-job-tab');
+            jobProfile.classList.add('active-job');
+        });
+    }
+    }
+    jobProfileChange()
